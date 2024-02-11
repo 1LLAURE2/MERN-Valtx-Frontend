d616f8649ad44c8f131881e5ac7bef5654744b27
1LLAURE2 <josellaurellapo@gmail.com>
2024-02-11 16:14:05 -0500

    Edicion completa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route,useParams} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import './App.css';
 
@@ -10,13 +10,17 @@ import CreateNote from './components/CreateNote';
 import CreateUser from './components/CreateUser';
 
 function App() {
+  //TODO: Las 2 lineas posteriores son necesarias para enviar el Id al componente 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const parm_id = () => <CreateNote params={useParams()} />;
+
   return (
     <Router>
       <Navigation></Navigation>
       <div className='container p-4'>
         <Routes>
           <Route path='/' exact Component={NotesList}/> 
-          <Route path='/edit/:id' Component={CreateNote}/> 
+          <Route path='/edit/:id' Component={parm_id} /> 
           <Route path='/create' Component={CreateNote}/> 
           <Route path='/user' Component={CreateUser}/> 
         </Routes>
--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -2,23 +2,46 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-
+//
 
 export default class CreateNote extends Component {
+    
     state={
         users:[],
         usuSeleccionado:'',
         txt_titulo:'',
         txt_contenido:'',
-        date:new Date()
+        date:new Date(),
+        editing:false,
+        _id:''
     }
 
     async componentDidMount(){
+
         const res=await axios.get('http://127.0.0.1:4000/api/usuarios');
         this.setState({
             users:res.data.map(user=>user.username),
             usuSeleccionado:res.data[0].username
-        }) 
+        })
+
+        if(this.props && this.props.params){
+            const IdentificadorNota=this.props.params.id
+            console.log('ID: '+this.props.params.id);
+            // console.log('Identificador: '+IdentificadorNota)
+            const res=await axios.get('http://127.0.0.1:4000/api/notas/'+IdentificadorNota)
+            this.setState({
+                txt_titulo:res.data.title,
+                txt_contenido:res.data.content,
+                date:new Date(res.data.date),
+                usuSeleccionado:res.data.author,
+                editing:true,
+                _id:IdentificadorNota
+            })
+            console.log('Autor: '+ this.state.usuSeleccionado)
+        }else{
+            console.log('NO HAY ID')
+        }
+        
         //console.log(this.state.users)
         
     }
@@ -31,8 +54,14 @@ export default class CreateNote extends Component {
             date:this.state.date,
             author:this.state.usuSeleccionado
         }
+      
+        if(this.state.editing===true){
+            await axios.put('http://127.0.0.1:4000/api/notas/'+this.state._id,nuevaNota);
+        }else{
+            await axios.post('http://127.0.0.1:4000/api/notas',nuevaNota);
+        }
         //TODO:INVOCO AL ENDPOINT, ENVIANDOLE EL CUERPO DE LA ESTRUCUTRA A GUARDAR
-        await axios.post('http://127.0.0.1:4000/api/notas',nuevaNota);
+        
         window.location.href='/';
         // console.log(this.state.txt_titulo,this.state.txt_contenido)
     }
@@ -46,7 +75,7 @@ export default class CreateNote extends Component {
         // console.log(e.target.name,e.target.value)
     }
     onChangeDate=date=>{
-        this.setState({date})
+        this.setState({date});
     }
     render() {
         return (
@@ -57,8 +86,9 @@ export default class CreateNote extends Component {
                 <div className='form-group'>
                     <select
                     className='form-control'
-                    name="usuarioSeleccionado"
+                    name="usuSeleccionado"
                     onChange={this.onInputChange}
+                    value={this.state.usuSeleccionado}
                     >
                         {
                             this.state.users.map(us=>
@@ -71,7 +101,14 @@ export default class CreateNote extends Component {
                     </select>
                 </div>
                 <div className='form-group mt-2'>
-                    <input type='text' className='form-control' placeholder='Ingrese Titulo' name='txt_titulo' onChange={this.onInputChange} required/>
+                    <input 
+                    type='text' 
+                    className='form-control' 
+                    placeholder='Ingrese Titulo' 
+                    name='txt_titulo' 
+                    onChange={this.onInputChange} 
+                    value={this.state.txt_titulo}
+                    required/>
                 </div>
                 <div className='form-group mt-2'>
                     <textarea
@@ -79,6 +116,7 @@ export default class CreateNote extends Component {
                         className='form-control'
                         placeholder='Ingrese el Contenido'
                         onChange={this.onInputChange}
+                        value={this.state.txt_contenido}
                         required
                     ></textarea>
                 </div>
